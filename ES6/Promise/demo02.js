1589bb93d34584895a1de0075237d0f2b2867dff
JasonChi <wb-qyp273848@antfin.com>
2019-04-18 00:09:26 +0800

    ss

--- ES6/Promise/demo02.js
+++ ES6/Promise/demo02.js
@@ -1,1 +1,9 @@
-console.log(global);
+const proxy = new Proxy({}, {
+    get: function (tartget, property) {
+        return 35;
+    }
+});
+
+console.log(proxy.time);
+console.log(proxy.name);
+console.log(proxy.title);
